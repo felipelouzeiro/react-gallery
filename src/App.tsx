97005abcff8e00ad979ff64be4b6f9c0596d1576
felipe <fl.louzeiro@gmail.com>
2022-08-23 23:05:08 -0300

    ✨ feat:  criando inserção de arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, FormEvent } from 'react';
+import * as C from './App.styles';
+import { PhotoCard } from './components/PhotoCard';
+import * as ServicesPhotos from './services/photos';
+import { Photo } from './services/types/Photo';
 
-function App() {
+export const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+
+      setPhotos(await ServicesPhotos.getAll());
+
+      setLoading(false);
+    };
+
+    getPhotos();
+  }, []);
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    console.log('AQ', file);
+    if (file && file.size > 0) {
+      setUploading(true);
+
+      let result = await ServicesPhotos.insert(file);
+
+      setUploading(false);
+
+      if (result instanceof Error) {
+        alert(`${result.name} - $${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <C.Container>
+      <C.Area>
+        <C.Header>Upload Gallery</C.Header>
+        <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type={'file'} name="image" />
+          <input type={'submit'} value="Enviar" />
+          {uploading && <span>enviando..</span>}
+        </C.UploadForm>
 
-export default App;
+        {loading && (
+          <C.ScreenMessage>
+            <div className="emoji">🕵️</div>
+            <div>Procurando fotos..</div>
+          </C.ScreenMessage>
+        )}
+
+        {!loading && photos.length > 0 && (
+          <C.PhotoList>
+            {photos.map((photo, index) => (
+              <PhotoCard key={index} name={photo.name} url={photo.url} />
+            ))}
+          </C.PhotoList>
+        )}
+
+        {!loading && photos.length === 0 && (
+          <C.ScreenMessage>
+            <div className="emoji">🤦‍♂️</div>
+            <div>Você não possui fotos cadastradas!</div>
+          </C.ScreenMessage>
+        )}
+      </C.Area>
+    </C.Container>
+  );
+};
